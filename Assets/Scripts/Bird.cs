1e0c30aa5c8dfa017722c95b8aea839dac040ddb
Rom Golovin <romgol@post.bgu.ac.il>
2022-07-27 00:37:10 +0300

    added movement

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,16 +7,28 @@ public class Bird : MonoBehaviour
 {
     [SerializeField] float _launchForce = 500;
     [SerializeField] float _maxDragDistance = 5;
+    [SerializeField] float _flySpeed = 4f;
 
+    enum State
+    {
+        IdleAscend,
+        IdleDescend,
+        Ascend,
+        Descend
+    }
 
     Vector2 _startPosition;
     Rigidbody2D _rigidbody2D;
     SpriteRenderer _spriteRenderer;
+    State _state;
+    float _upperScreenBound = 4.6f;
+    float _lowerScreenBound = -2.6f;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _state = State.IdleDescend;
     }
     // Start is called before the first frame update
     void Start()
@@ -39,7 +51,8 @@ public class Bird : MonoBehaviour
         _rigidbody2D.isKinematic = false;
         _rigidbody2D.AddForce(direction * _launchForce);
 
-        _spriteRenderer.color = Color.white;    
+        _spriteRenderer.color = Color.white;
+
     }
 
     void OnMouseDrag()
@@ -60,9 +73,76 @@ public class Bird : MonoBehaviour
 
         _rigidbody2D.position = desiredPosition;
     }
+
+    void FlyUp()
+    {
+        if (transform.position.y < _upperScreenBound)
+        {
+            transform.Translate(Vector2.up * _flySpeed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            _state = State.IdleAscend;
+            transform.Rotate(0.0f, 0.0f, -30f, Space.Self);
+        }
+    }
+
+    void FallDown()
+    {
+        if (transform.position.y > _lowerScreenBound)
+        {
+            transform.Translate(Vector2.down * _flySpeed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            _state = State.IdleDescend;
+            transform.Rotate(0.0f, 0.0f, 30f, Space.Self);
+        }
+    }
+
+    void StopMoving()
+    {
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (_state)
+            {
+                case State.IdleDescend:
+                    transform.Rotate(0.0f, 0.0f, 30f, Space.Self);
+                    _state = State.Ascend;
+                    break;
+                case State.Ascend:
+                    transform.Rotate(0.0f, 0.0f, -30f, Space.Self);
+                    _state = State.IdleAscend;
+                    break;
+                case State.IdleAscend:
+                    transform.Rotate(0.0f, 0.0f, -30f, Space.Self);
+                    _state = State.Descend;
+                    break;
+                case State.Descend:
+                    transform.Rotate(0.0f, 0.0f, 30f, Space.Self);
+                    _state = State.IdleDescend;
+                    break;
+            }
+        }
+
+        if(_state == State.Ascend)
+        {
+            FlyUp();
+        }
+        else if(_state == State.Descend)
+        {
+            FallDown();
+        }
+        else
+        {
+            StopMoving();
+        }
         
     }
 
